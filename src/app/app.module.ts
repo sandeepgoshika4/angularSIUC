5638140f0c42bd1014265d4860ba564b381bc79f
sandeep goshika4 <sandeep@falconlinkapp.com>
2022-08-05 23:41:57 -0500

    Second Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from "@angular/forms"
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+name = '';
+email = '';
+age = '';
+phone = '';
+address = '';
+comment = ''; 
+  constructor(private http:HttpClient) { }
+  submit(){
+    
+    // if(this.name.length < 3){
+    //   alert("please provide correct first name");
+    //   return;
+    // }
+    if(this.email.length < 3){
+      var emailCheckRegEx = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        console.log(this.email);
+        if(this.email.length > 0 && !emailCheckRegEx.test(this.email)) {
+            alert('Please enter a valid email');
+            return;
+        }
+    }
+    if(this.phone.length < 3){
+      var phonenoRegEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+      console.log(this.phone)
+      if(this.phone.length > 0 && !phonenoRegEx.test(this.phone))
+          {
+          alert("Please enter the phone number format");
+           return;
+          };
+    }
+    if(this.address.length < 7){
+      alert("please provide correct address");
+      return;
+    }
+
+    
+
+    let obj = {
+      name : this.name,
+      email : this.email,
+      phone: this.phone,
+      age: this.age,
+      address: this.address,
+      comment: this.comment
+    }
+
+    console.log(obj);
+
+    this.http.post("http://localhost:3000/",obj).subscribe((result) => {});
+
+    alert( ' Thank you for submitting the form.');
+
+    this.name = '';
+    this.email = '';
+    this.phone = '';
+    this.age = '';
+    this.address = ''
+    this.comment = '';
+    
+  }
+}
